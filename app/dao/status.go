4ca06b1874d6d0073727607fba0273a20f1a2310
nonrita <2220025@ecc.ac.jp>
2024-08-07 15:57:09 +0900

    add FindByIDを実装

--- app/dao/status.go
+++ app/dao/status.go
@@ -2,6 +2,8 @@ package dao
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
 	"yatter-backend-go/app/domain/object"
 	"yatter-backend-go/app/domain/repository"
@@ -19,6 +21,20 @@ var _ repository.Status = (*status)(nil)
 
 func NewStatus(db *sqlx.DB) *status {
 	return &status{db: db}
+}
+
+func (s *status) FindByID(ctx context.Context, id int) (*object.Status, error) {
+	entity := new(object.Status)
+	err := s.db.QueryRowxContext(ctx, "select * from status where id = ?", id).StructScan(entity)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+
+		return nil, fmt.Errorf("failed to find account from db: %w", err)
+	}
+
+	return entity, nil
 }
 
 func (s *status) Create(ctx context.Context, tx *sqlx.Tx, sta *object.Status) error {
