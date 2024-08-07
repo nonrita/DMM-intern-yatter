d5f828953043ab79cc05cb060ee0093d17052ebe
nonrita <2220025@ecc.ac.jp>
2024-08-07 17:19:05 +0900

    add timelinesのrouterを追加しました

--- app/handler/router.go
+++ app/handler/router.go
@@ -9,6 +9,7 @@ import (
 	"yatter-backend-go/app/handler/auth"
 	"yatter-backend-go/app/handler/health"
 	"yatter-backend-go/app/handler/statuses"
+	"yatter-backend-go/app/handler/timelines"
 	"yatter-backend-go/app/usecase"
 
 	"github.com/go-chi/chi/v5"
@@ -35,6 +36,7 @@ func NewRouter(au usecase.Account, ar repository.Account, su usecase.Status, sr 
 	r.Mount("/v1/statuses", statuses.NewRouter(ar, su, sr))
 	r.Mount("/v1/health", health.NewRouter())
 	r.Mount("/v1/auth", auth.NewRouter(ar))
+	r.Mount("/v1/timelines", timelines.NewRouter())
 
 	return r
 }
