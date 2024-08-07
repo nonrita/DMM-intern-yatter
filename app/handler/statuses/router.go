c42807af55a94dd5bd870d5d75f47206e3fddd89
nonrita <2220025@ecc.ac.jp>
2024-08-07 15:49:48 +0900

    add handlerにstatusUsecaseを追加

--- app/handler/statuses/router.go
+++ app/handler/statuses/router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"yatter-backend-go/app/domain/repository"
 	"yatter-backend-go/app/handler/auth"
+	"yatter-backend-go/app/usecase"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -11,17 +12,18 @@ import (
 // Implementation of handler
 type handler struct {
 	ar repository.Account
+	su usecase.Status
 }
 
 // Create Handler for `/v1/statuses/`
-func NewRouter(ar repository.Account) http.Handler {
+func NewRouter(ar repository.Account, su usecase.Status) http.Handler {
 	r := chi.NewRouter()
 
 	// r.Group()により、特定のグループに対してミドルウェアを適用する
 	// グループに対して適用されたミドルウェアは、そのグループに属する全てのエンドポイントに対して適用される
 	r.Group(func(r chi.Router) {
 		// リクエストの認証を行う
-		h := &handler{ar}
+		h := &handler{ar, su}
 		r.Get("/{id}", h.Get)
 		r.Group(func(r chi.Router) {
 			r.Use(auth.Middleware(ar))
