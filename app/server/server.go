6651df39e9d682ba6da9e68996b6899644698907
nonrita <2220025@ecc.ac.jp>
2024-08-07 15:59:42 +0900

    add statusのレポジトリをrouterに追加で渡す

--- app/server/server.go
+++ app/server/server.go
@@ -30,7 +30,7 @@ func Run() error {
 	statusUsecase := usecase.NewStatus(db, dao.NewStatus(db))
 
 	r := handler.NewRouter(
-		accountUsecase, dao.NewAccount(db),statusUsecase,
+		accountUsecase, dao.NewAccount(db),statusUsecase, dao.NewStatus(db),
 	)
 
 	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGTERM, os.Interrupt)
